ece9c0d97ff29c62312299293dab7cec6d73f5bd
Ashaya01 <ashaysah@gmail.com>
2024-07-06 23:55:53 +0545

    Sidebar Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/Home'
@@ -6,11 +6,14 @@ import Video from './Pages/Video/Video'
 
 
 const App = () => {
+
+  const [sidebar, setSidebar] = useState(true);
+
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar}/>
         <Routes>
-          <Route path="/" element={<Home></Home>} />
+          <Route path="/" element={<Home sidebar={sidebar}></Home>} />
           <Route path="/video/:categoryId/:videoId" element={<Video></Video>} />
         </Routes>
     </div>
